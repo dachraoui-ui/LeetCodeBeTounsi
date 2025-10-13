799adf521c59ea45f03df80cf68e09bc3a2eaee9
ahmed dacharoui <dachraouia193@gmail.com>
2025-10-13 22:22:35 +0100

    add the first solution for the isPalindrome problem

--- 9-Palindrome Number.py
+++ 9-Palindrome Number.py
@@ -1,3 +1,14 @@
 class Solution(object):
     def isPalindrome(self, x):
-        x = 3
+        # solution 1 : in this approch 
+        # I convert the number to a string then i make a loop that loop through this string 
+        # from the end of it then for every iteration it adds a caracter but in the reverse 
+        # then compare them .
+        strX = str(x)
+        strComp = ''
+        for i in range(len(strX) -  1 , -1 ,-1 ) : 
+            strComp = strComp + strX[i] 
+        if strComp == strX : 
+            return True 
+        else :
+            return False 
