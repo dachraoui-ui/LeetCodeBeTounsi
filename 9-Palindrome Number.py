f3ec3891a7b1a11df3cb9a3e9da13c76c822b91c
ahmed dacharoui <dachraouia193@gmail.com>
2025-10-13 22:23:59 +0100

    Update comments to clarify time and memory complexity for isPalindrome method

--- 9-Palindrome Number.py
+++ 9-Palindrome Number.py
@@ -4,6 +4,8 @@ class Solution(object):
         # I convert the number to a string then i make a loop that loop through this string 
         # from the end of it then for every iteration it adds a caracter but in the reverse 
         # then compare them .
+        # time complexity O(n)
+        # memory complexity O(n)
         strX = str(x)
         strComp = ''
         for i in range(len(strX) -  1 , -1 ,-1 ) : 
