3f5cb3c356e84ddc3923b6fbafba44d453eb08eb
ahmed dacharoui <dachraouia193@gmail.com>
2025-11-19 10:15:57 +0100

    Refactor findFinalValue method to improve clarity and add comments on time and space complexity

--- LeetCode/2154. Keep Multiplying Found Values by Two.py
+++ LeetCode/2154. Keep Multiplying Found Values by Two.py
@@ -1,4 +1,6 @@
 class Solution(object):
+    # solution without the dict 
+    # time complexity O(n) and space O(n) 
     def findFinalValue(self, nums, original):
         found = True
         while found :
@@ -6,4 +8,17 @@ class Solution(object):
                 original = original * 2 
             else : 
                 found = False
+        return original 
+    
+
+    #this solution use the dict 
+    #but the first solution in better in time complexity and space 
+    def findFinalValue(self, nums, original):
+        nu = {i: nums[i] for i in range(len(nums))}
+        found = True  
+        while found : 
+            if original in nu.values():
+                original = original *2
+            else : 
+                found = False 
         return original 
