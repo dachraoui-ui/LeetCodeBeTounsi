f7d0c0bf06aafe6a6809914a5d96c2de5021c9ae
ahmed dacharoui <dachraouia193@gmail.com>
2025-11-26 17:40:46 +0100

    Add detailed explanation for Happy Number implementation

--- LeetCode/easy/201...300/202. Happy Number.py
+++ LeetCode/easy/201...300/202. Happy Number.py
@@ -12,4 +12,8 @@ class Solution(object):
                 res = res + int(i)**2
             n = str(res)
 
-# todo : explanation 
+# todo : explanation : 
+# so in this implementation i convert n to a string so i can perferm string and list actions on it 
+# so the condition of happy number is that each power 2 digit added together should give a 1 so the result should be 
+# 10eN . so the idea is performing the pw 2 of each digits and each number we get we put it in a list l[], 
+# and every time we get the res we check if it's in the l or not to not perform an endless loopp 👌
