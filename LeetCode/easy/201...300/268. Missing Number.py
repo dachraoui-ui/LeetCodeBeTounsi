5c52b9329c1deee218c3d0b700a4b76ce860eb82
ahmed dacharoui <dachraouia193@gmail.com>
2025-12-03 12:24:59 +0100

    Add optimized solution for Missing Number problem with O(n) time complexity and O(1) space

--- LeetCode/easy/201...300/268. Missing Number.py
+++ LeetCode/easy/201...300/268. Missing Number.py
@@ -13,3 +13,13 @@ class Solution:
 # just creating another array that contains -1 (they say all are positive)
 # then arrange them by index in the n_sort array and if we find -1 so that number 
 # is missing 
+class Solution:
+    def missingNumber(self, n: List[int]) -> int:
+        real_sum = len(n)
+        sum_n = 0 
+        for i in range(len(n)):
+            sum_n = sum_n + n[i]
+            real_sum = real_sum + i
+        return real_sum - sum_n
+     
+# this is a solution of O(n) time complexity and O(1) space 
