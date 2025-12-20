e9d4bbc3668f7ec42d5a0c1faa061e5a6fa1a470
ahmed dacharoui <dachraouia193@gmail.com>
2025-12-20 21:49:17 +0100

    implement another solution (mathematical one)

--- LeetCode/easy/301...400/367. Valid Perfect Square.py
+++ LeetCode/easy/301...400/367. Valid Perfect Square.py
@@ -19,3 +19,16 @@ class Solution:
 # if less we goes to right and make it bigger .
 # this solution have O(log(n)) time , and O(1)  no extra space 
 # log n cause each time we get smaller part to search on it 
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        x = num
+        while (x * x > num) :
+            x = (x + num // x) // 2
+        return x * x == num
+    
+# now a pure mathematical solution using Newton method raphson for more undrestanding
+# visit : https://en.wikipedia.org/wiki/Newton%27s_method
+# in sumary this newton method using the formula (x + num // x) // 2
+# to each time get the best approximate for the root of that number 
+# the main idea is like that and finally we return if the end result power 2 equal to num it self 
